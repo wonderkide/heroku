5d62eae8d80e7a50a8d210b1e3e4a0a0eb49f74e
Den <korawit.ri@gmail.com>
2019-06-21 17:23:31 +0700

    add

--- webhook/exam/image.php
+++ webhook/exam/image.php
@@ -9,7 +9,7 @@ use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
 
 $access_token = 'qvQckpPL0EbDkJC1xU3T0sT+sr7MRks2yK48ApMDH8il8ZmyYV9huFBJOdvlVZMa8O0cziiS51ZiUdzRRBJpZQllAgSLN6ln6+YhmaDHWvyDNq/rto0PZbE263pQD5C7x7/F2JZqA0Dcib0fOFJTIQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '234c26728e0c47f150cbf4ed83de57de';
-$idPush = 'U433d8f17c86125016456c828eecb4381';
+$idPush = 'C57f74162e12d08a9085753349db78572';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
