860aaa8045a414e459f82cc7832b4f4271976995
Den <korawit.ri@gmail.com>
2019-06-21 17:22:04 +0700

    add

--- webhook/exam/image.php
+++ webhook/exam/image.php
@@ -5,6 +5,7 @@ require "vendor/autoload.php";
 use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
 
 $access_token = 'qvQckpPL0EbDkJC1xU3T0sT+sr7MRks2yK48ApMDH8il8ZmyYV9huFBJOdvlVZMa8O0cziiS51ZiUdzRRBJpZQllAgSLN6ln6+YhmaDHWvyDNq/rto0PZbE263pQD5C7x7/F2JZqA0Dcib0fOFJTIQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '234c26728e0c47f150cbf4ed83de57de';
@@ -21,14 +22,7 @@ $replyData = new TemplateMessageBuilder('Image Carousel',
                         'Uri Template', // ข้อความแสดงในปุ่ม
                         'https://www.google.com'
                     )
-                ),
-                /*new ImageCarouselColumnTemplateBuilder(
-                    'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
-                    new UriTemplateActionBuilder(
-                        'Uri Template', // ข้อความแสดงในปุ่ม
-                        'https://www.ninenik.com'
-                    )
-                )*/                                   
+                ),                       
             )
         )
     );
